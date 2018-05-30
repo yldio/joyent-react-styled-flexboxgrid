2bdcfbcedabb043ebc3fbf774f33da8631b76289
Sérgio Ramos <mail@sergioramos.me>
2018-05-30 16:10:57 +0100

    fix strings as props

--- src/components/Col.js
+++ src/components/Col.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import isInteger from 'lodash.isinteger';
+import isNumber from 'is-number';
 
 import config, { DIMENSION_NAMES } from '../config';
 
@@ -20,18 +21,9 @@ const Col = styled.div`
   padding-left: ${p => config(p).gutterWidth / 2}rem;
   display: block;
 
-  ${p =>
-    p.flex
-      ? `
-    display: flex;
-  `
-      : ''};
+  ${p => (p.flex ? `display: flex;` : '')};
 
-  ${p =>
-    p.reverse &&
-    `
-    flex-direction: column-reverse;
-  `}
+  ${p => p.reverse && `flex-direction: column-reverse;`};
 
   ${p =>
     Object.keys(p)
@@ -39,37 +31,27 @@ const Col = styled.div`
       .sort(
         (k1, k2) => DIMENSION_NAMES.indexOf(k1) - DIMENSION_NAMES.indexOf(k2)
       )
-      .map(
-        k =>
-          config(p).media[k]`${
-            isInteger(p[k])
-              ? // Integer value
-                `
+      .map(k => {
+        const pk = isNumber(p[k]) ? Number(p[k]) : p[k];
+
+        return config(p).media[k]`
+      ${isInteger(pk) ? `
         flex-basis: ${100 / config(p).gridSize * p[k]}%;
         max-width: ${100 / config(p).gridSize * p[k]}%;
-      `
-              : // Boolean
-                p[k]
-                ? // Auto-width
-                  `
-          flex-grow: 1;
-          flex-basis: 0;
-          max-width: 100%;
-        `
-                : // Hide element
-                  'display: none;'
-          }`
-      )}
-
-  ${p =>
+      ` : Boolean(p[k]) ? `
+        flex-grow: 1;
+        flex-basis: 0;
+        max-width: 100%;
+      ` : 'display: none;'}
+    `;
+      })} ${p =>
     Object.keys(p)
       .filter(k => offsetProps.indexOf(k) !== -1)
       .map(
-        k =>
-          config(p).media[k.replace(/Offset$/, '')]`
-        margin-left: ${100 / config(p).gridSize * p[k]}%;
-      `
-      )}
+        k => config(p).media[k.replace(/Offset$/, '')]`
+    margin-left: ${100 / config(p).gridSize * p[k]}%;
+  `
+      )};
 `;
 
 Col.displayName = 'Col';
